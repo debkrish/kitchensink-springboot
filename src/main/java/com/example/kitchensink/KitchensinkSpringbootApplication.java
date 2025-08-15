6cf14e2d44c10ce390576e6c562b02381611c9f4
Krishnendu Debnath <deb.krish93@gmail.com>
2025-08-15 22:49:58 +0530

    Initial commit: kitchensink-springboot with MongoDB

---
+++ src/main/java/com/example/kitchensink/KitchensinkSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.kitchensink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchensinkSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchensinkSpringbootApplication.class, args);
+	}
+
+}
